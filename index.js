c67c56d2a67ee7260e1db4f380641501ec606c8c
Jagrut Sharma <jagrut.sharma1@gmail.com>
2023-02-19 19:30:21 +0530

    question-1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hi there");
+// Question-1
+// Problem Statement: Given an integer N , write program to count number of digits in N.
+
+// Examples:
+
+// Example 1:
+// Input: N = 12345
+// Output: 5
+// Explanation: N has 5 digits
+
+const readlineSync = require("readline-sync");
+const numEntered = readlineSync.question(`Hi, Enter a number: \n`);
+// console.log(numEntered);
+let length = 0;
+for (let num of numEntered) {
+  if (num) length++;
+}
+console.log(length);
