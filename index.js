8dc262eb563728a30f233d32e0fd8a967a5e07da
Jagrut Sharma <jagrut.sharma1@gmail.com>
2023-02-26 17:51:50 +0530

    two sums and target

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
+// Find two sums:
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+//* SOLUTION:
+
+// if 9 - 2 = present return its value(index) and current index(i)
+
+const twoSum = function (nums, target) {
+  const arr = nums;
+  const targetSum = target;
+  const obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    // console.log(obj[targetSum - arr[i]]);
+    if (obj[targetSum - arr[i]] !== undefined) {
+      // checking for diff b/w target and current ele present in obj
+      return [obj[targetSum - arr[i]], i];
+    }
+    obj[arr[i]] = i; // storing element as key and index as value.
+    // console.log(obj);
+  }
+};
+
+// nums = [2,7,11,15], target = 9
+console.log(twoSum([2, 7, 11, 15], 9));
+// console.log(twoSum([3, 2, 4], 6));
+
+/*
 // Find all divisors or sum of all divisors:
 
 function findDivisor(n) {
